26eaafaa72a62c987eff45f889da78c5574719c1
TvaliashviliSandro <129891031+TvaliashviliSandro@users.noreply.github.com>
2024-02-26 02:07:59 +0400

    upgrading the writer system

--- main.py
+++ main.py
@@ -11,31 +11,39 @@ client = Client(email, password)
 target_user_id # = Include target user id.
 filename = "msges.txt"
 
+fetched_user_ids = []  # List to store fetched user IDs
+
 looper = True
 
-# Infinite loop to run the program nonstop 
+# Infinite loop to run the program nonstop
+while looper:
+    
+    consoletxt = input("input the console command (e.g., msgwriter, getinfo, logout): ")
 
-while looper == True:
-        consoletxt = str(input("input the console command (f.e: msgwriter, getinfo, logout): "))
-
-        # Message logger
-        if consoletxt == "msgwriter":
+    # Message logger
+    if consoletxt == "msgwriter":
+        if fetched_user_ids:
+            target_user_id = input("Enter the user ID of the person whose messages you want to fetch: ")
+            if target_user_id in fetched_user_ids:
                 msgwriter.get_and_save_conversation(client, target_user_id)
+            else:
+                print("Invalid user ID. Please use 'getinfo' command to fetch user IDs.")
+        else:
+            print("No user IDs fetched yet. Please use 'getinfo' command first.")
 
-        # Get certain peples user ID-s list corresponding to all the searched accounts
-        if consoletxt == "getinfo":
-                name = str(input("input name and surname of a person: "))
-                users = client.searchForUsers(name)
-                for i in range(0, len(users)):
-                        user = users[i]
-                        print(user.uid)
-        
-        # Log out function (in proggress / bug with logging out)
-        if consoletxt == "logout":
-                try:
-                        client.logout()
-                        print("Logout successful.")
-                        looper = False
-                except AttributeError:
-                         print("Failed to logout. Please try again later.")
+    # Get certain people's user ID-s list corresponding to all the searched accounts
+    elif consoletxt == "getinfo":
+        name = input("input name and surname of a person: ")
+        users = client.searchForUsers(name)
+        for user in users:
+            print(user.uid)
+            fetched_user_ids.append(user.uid)  # Add fetched user ID to the list
 
+    # Log out function
+    elif consoletxt == "logout":
+        try:
+            client.logout()
+            print("Logout successful.")
+            looper = False
+        except AttributeError:
+            print("Failed to logout. Please try again later.")
